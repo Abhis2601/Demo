ad17ce8278af7c69f3cd62e3b5e4ce7b4a5c1d7c
Abhishek Singh Chopdar <singhabhi2601.com>
2023-06-30 18:52:27 +0530

    Modify Controller and add sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,49 @@
 class UsersController < ApplicationController
   def new
-    @user=User.new
+    # @users=User.new
   end
 
+  def register
+    @user=User.new
+  end
    
   def create_user
     @user=User.new(data)
     if @user.save
-      redirect_to  articles_form_path
+      redirect_to  users_new_path
     else
-      render 'new'
+      render 'register'
     end
+  end
+
+  def login_user
+    # @users=User.new(data)
+    puts "1"
+    if users = User.find_by(name: params[:name],password: params[:password])
+      session[:current_user_id] = users.id
+      redirect_to articles_form_path
+    end
+      # session[:current_user_id] = users.id
+      # Save the user ID in the session so it can be used in
+      # subsequent requests
+  end
+
+  def destroy
+    # Remove the user id from the session
+    session.delete(:current_user_id)
+    # Clear the memoized current user
+    @_current_user = nil
+    # flash[:notice] = "You have successfully logged out."
+    redirect_to users_new_path, notice:"You have successfully logged out.",status: :see_other
   end
 
   private
   def data
     params.require(:user).permit(:name,:password)
+  end
+
+  def current_user
+    @_current_user ||= session[:current_user_id] &&
+      User.find_by(id: session[:current_user_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   get 'users/new'
+  get '/users/register', to:"users#register"
   get 'sessions/new'
   get 'articles/form'
+
+  get 'articles/new_post' ,to: "articles#new_post"  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'articles/new_post' ,to: "articles#new_post"
    post '/create_post' ,to: 'articles#create_post'
    post '/create_user' ,to: 'users#create_user'
+   post '/login_user',to: 'users#login_user'
+
+
+   delete 'logout',to: 'users#destroy'
 end
